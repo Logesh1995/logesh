0dc32742b58eb009a05d97ea1a2b99ab48caa7ac
Logesh1995 <92039644+Logesh1995@users.noreply.github.com>
2021-10-06 21:13:08 +0530

    Assignment_2

---
+++ Assignment2_Task1.py
@@ -0,0 +1,19 @@
+#To print star
+i=5
+j=0
+while i > 0:
+    j = i
+    while j<6 :
+        print('*', end='')
+        j= j+1
+    print()
+    i= i-1
+k = 4
+l = 0
+while k > 0:
+     l = k
+     while l>0:
+         print ("*", end ='')
+         l= l-1
+     print()
+     k = k-1
---
+++ Assignment2_Task2.py
@@ -0,0 +1,5 @@
+# Write a Python program to reverse a word after accepting the input from the user.
+
+a= input("Enter the name:")
+
+print(a[::-1])
