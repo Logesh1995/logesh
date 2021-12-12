b5c4901f52337d4d53421d268607fa7b9fbd66fc
Logesh1995 <92039644+Logesh1995@users.noreply.github.com>
2021-12-12 17:35:10 +0530

    Add files via upload

---
+++ Ass_5_sentence generator.py
@@ -0,0 +1,8 @@
+sub = ["Americans ","Indians"]
+ver = ["play","watch"]
+obj = ["Baseball","Cricket"]
+
+for i in sub:
+    for j in ver:
+        for k in obj:
+            print (i + " " + j + " " + k)
